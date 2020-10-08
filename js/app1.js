dc44c408c6330c8e2fdf90c468b22b614548ddb3
Cristian T <Cristian.gorjeanul@gmail.com>
2020-10-08 14:20:10 +0300

    Add experience time line and hourglass

--- js/app1.js
+++ js/app1.js
@@ -44,14 +44,41 @@ document.addEventListener('DOMContentLoaded', function () {
   setInterval(changeSlides, slideTimer);
 
   //Button
-
-
   var button = document.querySelector('.button');
   var counter = 1;
   button.addEventListener('click', function () {
     document.querySelector('.button span').innerText = counter;
     counter++;
   });
+
+//Experience timeline
+  const items = document.querySelectorAll('#timeline li');
+
+  const isInViewport = el => {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
+  const run = () =>
+    items.forEach(item => {
+      if (isInViewport(item)) {
+        item.classList.add('show');
+      }
+    });
+
+  // Events
+  window.addEventListener('load', run);
+  window.addEventListener('resize', run);
+  window.addEventListener('scroll', run);
+
+
+
 
 
 }); 
