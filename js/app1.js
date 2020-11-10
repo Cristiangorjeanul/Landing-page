24ccc2fcce60595cad72cc36d024586247512bde
Cristian T <Cristian.gorjeanul@gmail.com>
2020-11-10 19:35:13 +0200

    Small changes

--- js/app1.js
+++ js/app1.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
   setTimeout(function () {
     var element = document.querySelector("body");
     element.classList.add("loaded");
-  }, 3500);
+  }, 1);
 
   //Greeting
   var date = new Date();
