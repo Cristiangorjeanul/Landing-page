419faa791ae306e211e6bb76b30e47c1c6ccf8c7
Cristian T <Cristian.gorjeanul@gmail.com>
2021-03-22 22:20:10 +0200

    Delete row slide animation

--- js/app2.js
+++ js/app2.js
@@ -1,36 +1,5 @@
 $(document).ready(function () {
 
-  // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on('click', function (event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 900, function () {
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-
-  $(window).scroll(function () {
-    $(".slideanim").each(function () {
-      var pos = $(this).offset().top;
-
-      var winTop = $(window).scrollTop();
-      if (pos < winTop + 600) {
-        $(this).addClass("slide");
-      }
-    });
-  });
 
   // Blink websites number
   function blink(selector) {
@@ -119,7 +88,6 @@ $(document).ready(function () {
         animate(iterationCount - 1)
     })
   }
-
 
 
   //Rotating picture at click
